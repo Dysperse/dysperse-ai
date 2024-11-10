a71ca1bb4285a6a449b2fb3962ca66e29d2736c1
Manu G <manusvathgurudath@gmail.com>
2024-11-10 11:19:16 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+from hugchat import hugchat
+from hugchat.login import Login
+import os
+from flask_cors import cross_origin
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+# Log in to HuggingChat and set up cookies
+EMAIL = "manusvathgurudath@gmail.com"
+PASSWD = "156140171aA!"
+cookie_path_dir = "./cookies/"  # Ensure a trailing slash
+sign = Login(EMAIL, PASSWD)
+cookies = sign.login(cookie_dir_path=cookie_path_dir, save_cookies=True)
+
+# Create the ChatBot
+chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+
+@app.route('/', methods=['POST'])
+@cross_origin()
+def chat():
+    data = request.json
+    user_message = data.get("message", "")
+
+    if not user_message:
+        return jsonify({"error": "No message provided"}), 400
+
+    # Generate response from HuggingChat
+    message_result = chatbot.chat(f"You are an AI which will split this task into smaller, manageable subtasks which are relevant to achieve the task.\n" 
+                                  f"You will provide data in a minified JSON format only, without any surrouding or extra text.\n" 
+                                  f"You must follow this schema: [ {{ \"title\": \"...\", \"description\": \"...\" }} ] \n" 
+                                  f"Do not create more than 5 subtasks. Keep titles and descriptions to the point. If a description is not necessary or repetitive, omit it. \n" 
+                                  f"Do not use extra whitespace. {user_message}")
+    message_str = message_result.wait_until_done()
+
+    return jsonify({"response": message_str})
+
+    ### CORS section
+    @app.after_request
+    def after_request_func(response):
+        origin = request.headers.get('Origin')
+        if request.method == 'OPTIONS':
+            response = make_response()
+            response.headers.add('Access-Control-Allow-Credentials', 'true')
+            response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+            response.headers.add('Access-Control-Allow-Headers', 'x-csrf-token')
+            response.headers.add('Access-Control-Allow-Methods',
+                                'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+            if origin:
+                response.headers.add('Access-Control-Allow-Origin', origin)
+        else:
+            response.headers.add('Access-Control-Allow-Credentials', 'true')
+            if origin:
+                response.headers.add('Access-Control-Allow-Origin', origin)
+
+        return response
+    ### end CORS section
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
